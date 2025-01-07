0d78d701d278d20a7b74b0d2aaded8acff76fb21
MuralitharanYoganath <muraliemailz@gmail.com>
2025-01-07 12:22:17 +0530

    Updated the Employee Details

--- employee-management/src/app/app-routing.module.ts
+++ employee-management/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
-const routes: Routes = [];
+// Define your routes table
+const routes: Routes = [
+  { path: 'employees', component: EmployeeListComponent },  // List all employees
+  { path: 'create-employee', component: CreateEmployeeComponent },  // Add employee form
+  { path: 'update-employee/:id', component: UpdateEmployeeComponent },  // Update employee form
+  { path: 'employee-details/:id', component: EmployeeDetailsComponent },  // View employee details
+  { path: '', redirectTo: '/employees', pathMatch: 'full' },  // Default route to employee list
+  { path: '**', redirectTo: '/employees' }  // Wildcard route for a 404 page (optional)
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)],  // Register the routes
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- employee-management/src/app/app.module.ts
+++ employee-management/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';  // Import the AppRoutingModule
+import { FormsModule } from '@angular/forms';  // Import the FormsModule
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';  // Import the CreateEmployeeComponent
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'employees', component: EmployeeListComponent },  // List all employees
+  { path: 'create-employee', component: CreateEmployeeComponent },  // Add employee form
+  { path: '', redirectTo: '/employees', pathMatch: 'full' },  // Default route to employee list
+  { path: '**', redirectTo: '/employees' }  // Wildcard route for a 404 page (optional)
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeDetailsComponent,
+    EmployeeListComponent,
+    UpdateEmployeeComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
